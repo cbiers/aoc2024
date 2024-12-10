56897981367d2c1e10e6516d6e45291d3c0a2800
cbiers <chris.biersbach@gmail.com>
2024-12-10 10:17:26 +0100

    Final refactoring

--- 10/10_1.py
+++ 10/10_1.py
@@ -1,13 +1,13 @@
-example = False
-input = open(f"10/{'ex' if example else 'in'}.txt", "r")
-
 def reachable(m, s, e):
     return s == e or any([reachable(m, c, e) 
     for c in [(s[0] + i, s[1] + j) for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]]
     if 0 <= c[0] < len(m) and 0 <= c[1] < len(m[0]) and m[c[0]][c[1]] == m[s[0]][s[1]] + 1])
 
-m = [[int(c) for c in line.strip()] for line in input.readlines()]
-zeroes = [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == 0]
-nines = [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == 9]
+def all_nums(m, n):
+    return [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == n]
 
-print([reachable(m, zero, nine) for zero in zeroes for nine in nines].count(True))
+example = False
+
+m = [[int(c) for c in line.strip()] for line in open(f"10/{'ex' if example else 'in'}.txt", "r").readlines()]
+
+print([reachable(m, zero, nine) for zero in all_nums(m, 0) for nine in all_nums(m, 9)].count(True))
--- 10/10_2.py
+++ 10/10_2.py
@@ -1,12 +1,10 @@
-example = False
-input = open(f"10/{'ex' if example else 'in'}.txt", "r")
-
 def count_trails(m, p):
     return 1 if m[p[0]][p[1]] == 9 else sum(count_trails(m, c) 
     for c in [(p[0] + i, p[1] + j) for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]]
     if 0 <= c[0] < len(m) and 0 <= c[1] < len(m[0]) and m[c[0]][c[1]] == m[p[0]][p[1]] + 1)
 
-m = [[int(c) for c in line.strip()] for line in input.readlines()]
-zeroes = [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == 0]
+example = False
 
-print(sum([count_trails(m, zero) for zero in zeroes]))
+m = [[int(c) for c in line.strip()] for line in  open(f"10/{'ex' if example else 'in'}.txt", "r").readlines()]
+
+print(sum([count_trails(m, zero) for zero in [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == 0]]))
