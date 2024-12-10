35c54af5f8bd614ef4a8d3f9f739e13436158c61
cbiers <chris.biersbach@gmail.com>
2024-12-10 09:26:27 +0100

    Heavy Refactor of Day 10

--- 10/10_1.py
+++ 10/10_1.py
@@ -1,37 +1,13 @@
 example = False
-s = "ex" if example else "in"
+input = open(f"10/{'ex' if example else 'in'}.txt", "r")
 
-def reachable(map, start, end):
-    if start == end:
-        return True 
-    candidates = [(start[0] + 1, start[1]), (start[0] - 1, start[1]), (start[0], start[1] + 1), (start[0], start[1] - 1)]
-    for candidate in candidates:
-        if not(candidate[0] < 0 or candidate[0] >= len(map) or candidate[1] < 0 or candidate[1] >= len(map[0])):
-            if map[candidate[0]][candidate[1]] == map[start[0]][start[1]] + 1:
-                if reachable(map, candidate, end):
-                    return True
-    return False
+def reachable(m, s, e):
+    return s == e or any([reachable(m, c, e) 
+    for c in [(s[0] + 1, s[1]), (s[0] - 1, s[1]), (s[0], s[1] + 1), (s[0], s[1] - 1)] 
+    if c[0] >= 0 and c[0] < len(m) and c[1] >= 0 and c[1] < len(m[0]) and m[c[0]][c[1]] == m[s[0]][s[1]] + 1])
 
-input = open(f"10/{s}.txt", "r")
+m = [[int(c) for c in line.strip()] for line in input.readlines()]
+zeroes = [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == 0]
+nines = [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == 9]
 
-map = []
-zeroes = []
-nines = []
-
-for line in input.readlines():
-    map.append([])
-    for c in line.strip():
-        map[-1].append(int(c))
-        if c == "0":
-            zeroes.append((len(map) - 1, len(map[-1]) - 1))
-        elif c == "9":
-            nines.append((len(map) - 1, len(map[-1]) - 1))
-
-res = 0
-
-for zero in zeroes:
-    for nine in nines:
-        if reachable(map, zero, nine):
-            res += 1
-
-print(res)
+print([reachable(m, zero, nine) for zero in zeroes for nine in nines].count(True))
--- 10/10_2.py
+++ 10/10_2.py
@@ -1,32 +1,12 @@
 example = False
-s = "ex" if example else "in"
+input = open(f"10/{'ex' if example else 'in'}.txt", "r")
 
-def count_trails(map, pos):
-    if map[pos[0]][pos[1]] == 9:
-        return 1
-    res = 0
-    candidates = [(pos[0] + 1, pos[1]), (pos[0] - 1, pos[1]), (pos[0], pos[1] + 1), (pos[0], pos[1] - 1)]
-    for candidate in candidates:
-        if not(candidate[0] < 0 or candidate[0] >= len(map) or candidate[1] < 0 or candidate[1] >= len(map[0])):
-            if map[candidate[0]][candidate[1]] == map[pos[0]][pos[1]] + 1:
-                res += count_trails(map, candidate)
-    return res
+def count_trails(m, p):
+    return 1 if m[p[0]][p[1]] == 9 else sum(count_trails(m, c) 
+    for c in [(p[0] + 1, p[1]), (p[0] - 1, p[1]), (p[0], p[1] + 1), (p[0], p[1] - 1)] 
+    if c[0] >= 0 and c[0] < len(m) and c[1] >= 0 and c[1] < len(m[0]) and m[c[0]][c[1]] == m[p[0]][p[1]] + 1)
 
-input = open(f"10/{s}.txt", "r")
+m = [[int(c) for c in line.strip()] for line in input.readlines()]
+zeroes = [(i, j) for i in range(len(m)) for j in range(len(m[0])) if m[i][j] == 0]
 
-map = []
-zeroes = []
-
-for line in input.readlines():
-    map.append([])
-    for c in line.strip():
-        map[-1].append(int(c))
-        if c == "0":
-            zeroes.append((len(map) - 1, len(map[-1]) - 1))
-
-res = 0
-
-for zero in zeroes:
-    res += count_trails(map, zero)
-
-print(res)
+print(sum([count_trails(m, zero) for zero in zeroes]))
