e8c0510cb194480f07154666353699cc6dd0e57f
cbiers <chris.biersbach@gmail.com>
2024-12-11 09:59:29 +0100

    Slight refactor

--- 11/11_2.py
+++ 11/11_2.py
@@ -1,31 +1,22 @@
 s = {c : {"count": 1, "rep": None} for c in open(f"11/{'ex' if False else 'in'}.txt", "r").readlines()[0].strip().split(" ")}
 
-for j in range(75):
+for _ in range(75):
     add = []
-    k = list(s.keys())
-    for v in k:
-        if s[v]["count"] == 0:
-            continue
+    for v in [k for k in s if s[k]["count"] > 0]:
         if s[v]["rep"] == None:
             if v == "0":
                 s[v]["rep"] = ["1"]
             elif len(v) % 2 == 0:
-                sec = v[len(v) // 2:].lstrip("0")
-                rep = [v[:len(v) // 2]] 
-                if sec == "":
-                    sec = "0"
-                rep.append(sec)
-                s[v]["rep"] = rep
+                s[v]["rep"] = [v[:len(v) // 2], v[len(v) // 2:].lstrip("0") if v[len(v) // 2:].lstrip("0") != "" else "0"]
             else:
                 s[v]["rep"] = [str(int(v) * 2024)]
         c = s[v]["count"]
         s[v]["count"] = 0
-        for r in s[v]["rep"]:
-            add.append((r, c))
+        add.extend([(r, c) for r in s[v]["rep"]])
     for a in add:
         if a[0] in s:
             s[a[0]]["count"] += a[1]
         else:
             s[a[0]] = {"count": a[1], "rep": None}
 
-print(sum(v["count"] for v in s.values() if v["count"] > 0))
+print(sum(v["count"] for v in s.values()))
