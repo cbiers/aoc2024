575542906814a8efe0d5cf2af74946c52ddd7590
cbiers <chris.biersbach@gmail.com>
2024-12-19 10:57:26 +0100

    further Refactoring

--- 19/19_1.py
+++ 19/19_1.py
@@ -2,9 +2,8 @@ example = False
 lines = open(f"19/{'ex' if example else 'in'}.txt", "r").readlines()
 
 def is_possible(pattern):
-    if pattern in cache:
-        return cache[pattern]
-    cache[pattern] = pattern == "" or any(pattern.startswith(stripe) and is_possible(pattern[len(stripe):]) for stripe in stripes)
+    if pattern not in cache:
+        cache[pattern] = pattern == "" or any(pattern.startswith(stripe) and is_possible(pattern[len(stripe):]) for stripe in stripes)
     return cache[pattern]
 
 stripes = lines[0].strip().split(", ")
--- 19/19_2.py
+++ 19/19_2.py
@@ -2,22 +2,19 @@ example = False
 lines = open(f"19/{'ex' if example else 'in'}.txt", "r").readlines()
 
 def is_possible(pattern):
-    if pattern in cache:
-        return cache[pattern]
-    cache[pattern] = pattern == "" or any(pattern.startswith(stripe) and is_possible(pattern[len(stripe):]) for stripe in stripes)
+    if pattern not in cache:
+        cache[pattern] = pattern == "" or any(pattern.startswith(stripe) and is_possible(pattern[len(stripe):]) for stripe in stripes)
     return cache[pattern]
 
 def combinations(pattern):
-    if pattern in comb_cache:
-        return comb_cache[pattern]
     if pattern == "":
         return 1
-    comb_cache[pattern] = sum([combinations(pattern[len(stripe):]) for stripe in stripes if pattern.startswith(stripe) and is_possible(pattern[len(stripe):])])
+    if pattern not in comb_cache:
+        comb_cache[pattern] = sum([combinations(pattern[len(stripe):]) for stripe in stripes if pattern.startswith(stripe) and is_possible(pattern[len(stripe):])])
     return comb_cache[pattern]
 
 stripes = lines[0].strip().split(", ")
 patterns = [line.strip() for line in lines[2:]]
-cache = {}
-comb_cache = {}
+cache, comb_cache = {}, {}
 
 print(sum(combinations(pattern) for pattern in patterns if is_possible(pattern)))
