4ff7e9f9b0258a32a5a85fecb4c0f85ba83ad706
cbiers <chris.biersbach@gmail.com>
2024-12-19 09:43:35 +0100

    further Refactoring

--- 19/19_1.py
+++ 19/19_1.py
@@ -4,15 +4,8 @@ lines = open(f"19/{'ex' if example else 'in'}.txt", "r").readlines()
 def is_possible(pattern):
     if pattern in cache:
         return cache[pattern]
-    if pattern == "":
-        cache[pattern] = True
-        return True
-    for stripe in stripes:
-        if pattern.startswith(stripe) and is_possible(pattern[len(stripe):]):
-            cache[pattern] = True
-            return True
-    cache[pattern] = False
-    return False
+    cache[pattern] = pattern == "" or any(pattern.startswith(stripe) and is_possible(pattern[len(stripe):]) for stripe in stripes)
+    return cache[pattern]
 
 stripes = lines[0].strip().split(", ")
 cache = {}
