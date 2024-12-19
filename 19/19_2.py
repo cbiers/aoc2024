621fa9af9f98b5bdf339d30caf78880bd89dd453
cbiers <chris.biersbach@gmail.com>
2024-12-19 11:57:50 +0100

    Improved variable naming

--- 19/19_2.py
+++ 19/19_2.py
@@ -2,16 +2,16 @@ example = False
 file = map(str.strip, open(f"19/{'ex' if example else 'in'}.txt", "r"))
 
 def is_possible(pattern):
-    if pattern not in cache:
-        cache[pattern] = pattern == "" or any(pattern.startswith(stripe) and is_possible(pattern[len(stripe):]) for stripe in stripes)
-    return cache[pattern]
+    if pattern not in valid_cache:
+        valid_cache[pattern] = pattern == "" or any(pattern.startswith(stripe) and is_possible(pattern[len(stripe):]) for stripe in stripes)
+    return valid_cache[pattern]
 
 def combinations(pattern):
-    if pattern not in comb_cache:
-        comb_cache[pattern] = sum(combinations(pattern[len(stripe):]) for stripe in filter(pattern.startswith, stripes))
-    return comb_cache[pattern]
+    if pattern not in combination_cache:
+        combination_cache[pattern] = sum(combinations(pattern[len(stripe):]) for stripe in filter(pattern.startswith, stripes))
+    return combination_cache[pattern]
 
 stripes = next(file).split(", ")
-cache, comb_cache = {}, {"": 1}
+valid_cache, combination_cache = {}, {"": 1}
 
 print(sum(combinations(pattern) for pattern in filter(is_possible, file)))
